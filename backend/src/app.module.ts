601bd19641fb7936eb03a95b77aafb3e47d09b02
Jonata <jonata.serpa@inatel.br>
2022-09-16 15:21:14 -0300

    feat(receipt): add crud receipt

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,12 +5,13 @@ import { UserModule } from './modules/user/user.module';
 import { CompanyModule } from './modules/company/company.module';
 import { CattlesModule } from './modules/cattles/cattles.module';
 import { PaymentModule } from './modules/payment/payment.module';
+import { ReceiptModule } from './modules/receipt/receipt.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule, 
-    UserModule, CompanyModule, CattlesModule, PaymentModule
+    UserModule, CompanyModule, CattlesModule, PaymentModule, ReceiptModule
   ],
   controllers: [],
   providers: [],
